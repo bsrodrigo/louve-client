94b2476831e2f5770abff0eca6b7e094619e5f71
Rodrigo Batista <rodrigo.bsilva@somoseducacao.com.br>
2024-09-25 23:45:10 -0300

    fix: removed api avances

--- src/modules/core/infra/api/avances-service.ts
+++ src/modules/core/infra/api/avances-service.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const avancesApi = axios.create({
   // baseURL: process.env.REACT_APP_API_HOST,
-  baseURL: "https://avances-service-production.up.railway.app",
 });
 
 avancesApi.interceptors.request.use((config) => {
